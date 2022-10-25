41929e69647b448e84e254f3a20610c47e286fd3
Luca Fracassetti <luca.fraca1998@gmail.com>
2022-10-25 16:25:18 +0200

    [devel] WIP on download withings

--- lib/bloc/detail_bloc/detail_bloc.dart
+++ lib/bloc/detail_bloc/detail_bloc.dart
@@ -1,14 +1,18 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:drift/drift.dart';
 import 'package:equatable/equatable.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:timerun/database/AppDatabase.dart';
 import 'package:timerun/model/device.dart';
+import 'package:withings_flutter/withings_flutter.dart';
 part 'detail_event.dart';
 part 'detail_state.dart';
 
 class DetailBloc extends Bloc<DetailEvent, DetailState> {
   AppDatabase db = GetIt.I<AppDatabase>();
+  SharedPreferences prefs = GetIt.I<SharedPreferences>();
   StreamSubscription? subStreamSession;
   DetailBloc(int id) : super(DetailStateLoading()) {
     Stream<List<Session>> streamSession = db.sessionsDao.watchSessionUser(id);
@@ -33,6 +37,42 @@ class DetailBloc extends Bloc<DetailEvent, DetailState> {
       },
     );
 
+    _withingsDownload(int idSession) async {
+      List<Interval> interv =
+          await db.intervalsDao.getIntervalBySession(idSession);
+      print(interv);
+      List<List<WithingsRatesCompanion>> withingsToSave =
+          List.generate(interv.length, (index) => []);
+      try {
+        for (int i = 0; i < interv.length; i++) {
+          WithingsMeasureGetIntradayactivityData getintradayactivity =
+              await WithingsMeasureGetIntradayactivityDataManager()
+                  .fetch(WithingsMeasureAPIURL.getIntradayactivity(
+            accessToken: prefs.getString('withingsAccessToken')!,
+            startdate: interv[i].startstimestamp,
+            enddate: interv[i].endtimestamp,
+            dataFields: 'heart_rate',
+          ));
+          if (getintradayactivity.status == 0) {
+            if (getintradayactivity.series!.isEmpty) {
+              print('Non ho valori per questo intervallo ${i + 1}');
+            } else {
+              getintradayactivity.series!.forEach((element) {
+                withingsToSave[i].add(WithingsRatesCompanion(
+                    idInterval: Value(interv[i].id),
+                    timestamp: Value(element.timestamp!),
+                    value: Value(element.heartRate!)));
+              });
+            }
+          }
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+
+    _fitbitDownload() {}
+
     on<DetailEventDownload>(
       (event, emit) async {
         print('Pressed download');
@@ -49,7 +89,10 @@ class DetailBloc extends Bloc<DetailEvent, DetailState> {
           if ((state as DetailStateDownloading).session1!.device1 ==
                   devices[1] ||
               (state as DetailStateDownloading).session1!.device2 ==
-                  devices[1]) {} //Withings
+                  devices[1]) {
+            await _withingsDownload(
+                (state as DetailStateDownloading).session1!.id);
+          } //Withings
 
         } else {
           if ((state as DetailStateDownloading).session2!.device1 ==
@@ -63,10 +106,10 @@ class DetailBloc extends Bloc<DetailEvent, DetailState> {
 
         }
 
-        await Future.delayed(Duration(seconds: 3));
+        /*await Future.delayed(Duration(seconds: 3));
         emit(DetailStateLoaded(
             session1: (state as DetailStateDownloading).session1,
-            session2: (state as DetailStateDownloading).session2));
+            session2: (state as DetailStateDownloading).session2));*/
       },
     );
   }
@@ -76,7 +119,3 @@ class DetailBloc extends Bloc<DetailEvent, DetailState> {
     return super.close();
   }
 }
-
-_withingsDownload() {}
-
-_fitbitDownload() {}
